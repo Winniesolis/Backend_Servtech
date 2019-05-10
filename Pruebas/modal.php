96f9ae42c1c2fb8c4762c19713a10149cf8658e6
WinnieSolis <winniesolis97@gmail.com>
2019-05-10 09:02:40 -0500

    Altas

---
+++ Pruebas/modal.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Backend │ ServTech</title>
+    <!-- style -->
+    <link rel="stylesheet" href="style-prueba.css">
+    <!-- font-awasome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+</head>
+<body>
+
+   
+        <h1>ventana modala</h1>
+        <a href="#modal" id="show-modal">Abrir Ventana</a>
+        <aside class="modal" id="modal">
+            <div class="content-modal">
+                <header>
+                    <a href="#" class="close-modal">X</a>
+                    <h2>Buen trabajo!</h2>
+                </header>
+                <article>
+                    <h2>buentrabajo2</h2>
+                </article>
+            </div>
+            <a href="#" class="btn-close-modal"></a>
+        </aside>
+   
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Backend │ ServTech</title>
+    <!-- style -->
+    <link rel="stylesheet" href="css/style-header.css">
+    <!-- font-awasome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+</head>
+<body>
+    <header>
+    <section class="principal">
+        <img src="img/logo-ST.PNG" alt="">
+    </section>
+    <nav>
+        <ul>
+            <li><a href=""><i class="fas fa-home p-ico"><p>Inicio</p></i></a></li>
+            <li><a href=""><i class="fas fa-user"><p>Usuarios</p></i></a></li>
+            <li><a href=""><i class="fas fa-laptop"><p>Productos</p></i></a></li>
+            <li><a href=""><i class="fas fa-handshake"><p>Servicios</p></i></a></li>
+            <li><a href=""><i class="fas fa-map-marker-alt"><p>Ubicacion</p></i></a></li>
+            <li><a href=""><i class="fas fa-file"><p>Reportes</p></i></a></li>
+        </ul>
+    </nav>
+    <section class="usuario">
+        <img src="img/winnie.png" alt="">
+        <p>Winnie Solis</p>
+        <p>Administrador</p>
+    </section>
+
+
+
+    </header>
+
+
+
+
+
+</body>
+</html>
